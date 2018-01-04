c3048143a0cc34ab51a98207706e25cbd6ac4bad
Nandira Paturohman <nandirasp@gmail.com>
2018-01-04 17:25:54 +0700

    navigation home

--- App.js
+++ App.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation'
+
+import HomeScreen from './screens/Home'
+import BrowseScreen from './screens/Browse'
+import SearchScreen from './screens/Search'
+import MyAccountScreen from './screens/Collection'
+
+const AppNavigator = StackNavigator({
+  Home: {
+    screen: HomeScreen
+  },
+  Browse: {
+    screen: BrowseScreen
+  },
+  Search: {
+    screen: SearchScreen
+  },
+  Collection: {
+    screen: MyAccountScreen
+  }
+})
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <AppNavigator/>
     );
   }
 }
